4359595970801cf2595ab216cc96ddf9f2a9011f
Jose Luis <jocorreamoreno@gmail.com>
2024-05-10 13:10:47 -0500

    Added .env loader

--- src/com/aluracursos/currencyconverter/util/EnvLoader.java
+++ src/com/aluracursos/currencyconverter/util/EnvLoader.java
@@ -1,2 +1,22 @@
-package com.aluracursos.currencyconverter.util;public class EnvLoader {
+package com.aluracursos.currencyconverter.util;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class EnvLoader {
+    public static void loadEnv(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("=", 2);
+                if (parts.length == 2) {
+                    String key = parts[0].trim();
+                    String value = parts[1].trim();
+                    System.setProperty(key, value);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
