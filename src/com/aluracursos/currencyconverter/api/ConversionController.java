1737c395ddcbf84f31079108a800641b6b95a396
Jose Luis <jocorreamoreno@gmail.com>
2024-05-15 23:46:07 -0500

    Implemented logic for conversion rate

--- src/com/aluracursos/currencyconverter/api/ConversionController.java
+++ src/com/aluracursos/currencyconverter/api/ConversionController.java
@@ -1,4 +1,24 @@
 package com.aluracursos.currencyconverter.api;
 
+import java.util.Map;
+
 public class ConversionController {
+    private final Map<String, Double> exchangeRates;
+
+    public ConversionController(Map<String, Double> exchangeRates) {
+        if (exchangeRates==null){
+            throw new NullPointerException("Can't initialize Conversion Controller: Exchange rate map is null");
+        }
+        this.exchangeRates = exchangeRates;
+    }
+
+    public double convert(double amount, String fromCurrency, String toCurrency) {
+        if (!exchangeRates.containsKey(fromCurrency) || !exchangeRates.containsKey(toCurrency)) {
+            throw new IllegalArgumentException("Currency code not found");
+        }
+
+        double amountInUSD = amount / exchangeRates.get(fromCurrency);
+
+        return amountInUSD * exchangeRates.get(toCurrency);
+    }
 }
